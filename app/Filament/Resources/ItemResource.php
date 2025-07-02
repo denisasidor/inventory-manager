27ea5891a517ed24538e785da3d2aedbee2c254a
denisasidor <denisasidor13@gmail.com>
2025-07-02 12:49:22 +0300

    Initial commit with Item resource

---
+++ app/Filament/Resources/ItemResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+
+use App\Filament\Resources\ItemResource\Pages;
+use App\Filament\Resources\ItemResource\RelationManagers;
+use App\Models\Item;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ItemResource extends Resource
+{
+    protected static ?string $model = Item::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            TextInput::make('name')
+                ->required()
+                ->label('Product Name'),
+
+            TextInput::make('quantity')
+                ->numeric()
+                ->required()
+                ->label('Quantity'),
+
+            TextInput::make('location')
+                ->required()
+                ->label('Location'),
+
+            TextInput::make('price')
+                ->numeric()
+                ->required()
+                ->label('Price'),
+        ]);
+    }
+
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->searchable()->sortable(),
+                TextColumn::make('quantity')->sortable(),
+                TextColumn::make('location'),
+                TextColumn::make('price')->money('RON'),
+                TextColumn::make('created_at')->dateTime()->label('Added On'),
+            ])
+            ->filters([])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListItems::route('/'),
+            'create' => Pages\CreateItem::route('/create'),
+            'edit' => Pages\EditItem::route('/{record}/edit'),
+        ];
+    }
+}
