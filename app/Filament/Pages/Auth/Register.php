38543ef73176c158b598dbd15276e6b5c4041ff4
denisasidor <denisasidor13@gmail.com>
2025-07-04 14:44:16 +0300

    Added registration field and create a company for user

---
+++ app/Filament/Pages/Auth/Register.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Filament\Pages\Auth;
+
+use App\Models\Company;
+use Filament\Forms\Components\TextInput;
+use Filament\Pages\Page;
+use Filament\Pages\Auth\Register as BaseRegister;
+
+class Register extends BaseRegister
+{
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getNameFormComponent(),
+                        $this->getEmailFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getPasswordConfirmationFormComponent(),
+                        $this->getCompanyFormComponent(),
+                    ])
+                    ->statePath('data'),
+            ),
+        ];
+    }
+
+    private function getCompanyFormComponent()
+    {
+        return TextInput::make('company_name')
+            ->label(__('Company Name'))
+            ->required()
+            ->maxLength(255)
+            ->autofocus();
+
+    }
+
+    protected function handleRegistration(array $data): \Illuminate\Database\Eloquent\Model
+    {
+        $companyId = Company::create(['name'=>$data['company_name']])->id;
+
+        return parent::handleRegistration([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => $data['password'],
+            'company_id' => $companyId,
+        ]);
+    }
+
+}
